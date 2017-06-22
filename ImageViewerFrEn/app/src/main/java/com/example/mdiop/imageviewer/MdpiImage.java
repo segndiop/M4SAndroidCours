a2c64938e579eabc8ee9617df1f7ac4fad7816d1
SegnDiop <diawma85@gmail.com>
2017-06-22 23:48:31 +0000

    FR EN

---
+++ ImageViewerFrEn/app/src/main/java/com/example/mdiop/imageviewer/MdpiImage.java
@@ -0,0 +1,21 @@
+package com.example.mdiop.imageviewer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mdiop on 22/06/2017.
+ */
+
+public class MdpiImage extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup parentViewGroup,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.mdpi_layout, parentViewGroup, false);
+
+        return rootView;
+    }
+}
